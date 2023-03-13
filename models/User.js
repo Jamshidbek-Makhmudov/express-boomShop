ac6b98a960d41eae73e6d64e233758bc068165eb
Jamshid Makhmudov <jamshid.makh94@gmail.com>
2023-03-14 06:58:03 +0900

    bcrypt hash password

---
+++ models/User.js
@@ -0,0 +1,16 @@
+//model papka nimaga kerak?
+//mongoDB da collectionlar ichida modellar boladi; userga tegishli, productga tegishli shularni saqlab turadi
+import { Schema, model } from "mongoose" //bu bizga 1ta model yaratishga yordam berdi
+//new Schemani parametrida object korinishida bizni user haqida backend malumotlarimiz boladi
+//bu yerda required:true degani, user sign-up qilayotganda shu keylar kiritilishi shart degani
+//unique:true degani esa, bir xil emailni 2marta kiritib bolmaydi degani
+
+const UserSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+})
+
+const User = model("User", UserSchema)
+export default User
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,8 @@
 import { Router } from "express"
+//backend modelimizni ovoldik
+import User from "../models/User.js"
+//bcrypt npm package bizga userdan keladigan passwordni hash qilish uchun kerak
+import bcrypt from "bcrypt"
 const router = Router()
 
 router.get("/login", (req, res) => {
@@ -18,8 +22,19 @@ router.post("/login", (req, res) => {
 })
 
 //register modulda buttonni bosgan about modulga jonatib yuboradi
-router.post("/register", (req, res) => {
-  console.log(req.body)
+router.post("/register", async (req, res) => {
+  //bcryptni sign-up qilishda ishlatish
+  const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+  // console.log(req.body)
+  const userData = {
+    firstName: req.body.firstname,
+    lastName: req.body.lastname,
+    email: req.body.email,
+    password: hashedPassword,
+  }
+  const user = await User.create(userData)
+  console.log(user)
   res.redirect("/about")
 })
 export default router
